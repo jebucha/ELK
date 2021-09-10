013ec63f9ebca100aabb28f553c9d3bb132034a8
John Buchanan <18007771+jebucha@users.noreply.github.com>
2021-09-10 09:37:36 -0500

    Create elasticsearch_only_rpm_install

---
+++ install_scripts/elasticsearch_only_rpm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Installation of Elasticsearch only dedicated master for RHEL/Cent/Rocky
+
+# Run as root
+
+# Download Elasticsearch
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.8.18.rpm
