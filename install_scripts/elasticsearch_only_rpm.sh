025e96506846318a00222a5e88a059ef8aabeab3
John Buchanan <18007771+jebucha@users.noreply.github.com>
2021-09-10 10:15:04 -0500

    Completed install script

--- install_scripts/elasticsearch_only_rpm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Installation of Elasticsearch only dedicated master for RHEL/Cent/Rocky
-
-# Run as root
-
-# Download Elasticsearch
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.8.18.rpm
---
+++ install_scripts/elasticsearch_only_rpm_install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Installation of Elasticsearch only dedicated master for RHEL/Cent/Rocky
+
+# Run as root
+
+# Download Elasticsearch
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.8.18.rpm
+
+# Install package
+rpm -ivh elasticsearch-6.8.18.rpm
+
+# backup the yml
+mv /etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml.orig
+
+
+# setup elasticsearch
+path="/etc/elasticsearch/"
+filename="elasticsearch.yml"
+strClusterNameOrig='#cluster.name: my-application'
+strClusterNameNew='cluster.name: graylog'
+strHostOrig='#network.host: 192.168.0.1'
+strHostNew='network.host: ["_local_","_site_"]'
+strPortOrig='#http.port: 9200'
+strPortNew='http.port: 9200'
+echo "node.master: true" >> $path/$filename
+echo "node.data: false" >> $path/$filename
+echo "node.ingest: false" >> $path/$filename
+sed -i "s|$strClusterNameOrig|$strClusterNameNew|g" $path/$filename
+sed -i "s|$strHostOrig|$strHostNew|g" $path/$filename
+sed -i "s|$strPortOrig|$strPortNew|g" $path/$filename
+
+# set the owner
+chown -R elasticsearch:elasticsearch /etc/elasticsearch/
+
+# enable the services
+systemctl daemon-reload
+systemctl enable elasticsearch.service
+
+# start the services
+systemctl start elasticsearch.service
+
+# delay for 60 seconds to let the services come up
+echo -e '60 second delay for services to start'
+sleep 60
+echo -e '60 second delay over'
+
+curl -XGET http://localhost:9200
+
+echo 'Done'
